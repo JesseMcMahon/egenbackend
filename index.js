e213c310c63a3b13a53e40e715fcf97ccebc55ae
Jesse Mcmahon <jessemcmahon@jesses-air.myfiosgateway.com>
2021-05-17 14:25:29 -0400

    add route for getting initial location

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const bodyParser = require("body-parser");
 const app = express();
 const axios = require("axios");
 const stringify = require("json-stringify-safe");
+const { response } = require("express");
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(bodyParser());
 
 app.use(cors());
 
@@ -21,6 +23,19 @@ app.get("/", async (req, res) => {
       }
     )
     .then((data) => res.send(stringify(data)));
+});
+
+app.get("/userlocation", async (req, res) => {
+  const { APIKEY, latitude, longitude } = req.query;
+  const response = [];
+  console.log(APIKEY, latitude, longitude);
+  await axios
+    .get(
+      `https://us1.locationiq.com/v1/reverse.php?key=${APIKEY}&lat=${latitude}&lon=${longitude}&format=json`
+    )
+    .then((res) => response.push(res.data));
+
+  res.send(response);
 });
 
 app.listen(PORT, () => {
